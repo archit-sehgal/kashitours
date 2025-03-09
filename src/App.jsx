5b70cc56e0fab1ecf2ad1a459e7b8147d40b296b
Archit Sehgal <architsehgal19@gmail.com>
2025-03-09 21:09:22 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { useState } from 'react';
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
-import './App.css'
-import Home from './components/Home';
-import Services from './components/Services';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Services from "./components/Services";
 
 function App() {
+  useEffect(() => {
+    const disableRightClick = (event) => event.preventDefault();
+    const disableShortcuts = (event) => {
+      if (event.ctrlKey && event.key.toLowerCase() === "u") {
+        event.preventDefault();
+      }
+      if (event.keyCode === 123) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener("contextmenu", disableRightClick);
+    document.addEventListener("keydown", disableShortcuts);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+      document.removeEventListener("keydown", disableShortcuts);
+    };
+  }, []);
 
   return (
     <Router>
-     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/services' element={<Services/>}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
